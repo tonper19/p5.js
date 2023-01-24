2bd528a7b811fe087d58dd854ffeb802ce38f4e2
Miguel Perez <miguel.p.donate@gmail.com>
2023-01-24 19:55:22 +0100

    24/01/2023 progress

--- sketch.js
+++ sketch.js
@@ -44,35 +44,115 @@
 
 
 
+ // This is to create the canvas with the square 
 
-let squareSize;
-let lineWidth;
+// let squareSize;
+// let lineWidth;
+
+// function setup() {
+//   createCanvas(500, 400);
+//   //squareSize = random(50, 100);
+//   lineWidth = random(4,28);
+//   background(100);
+// }
+
+// function draw() {
+//   //background(100);
+//   squareSize = random(5, 250);
+
+//   rectMode(CENTER);
+//   strokeWeight(lineWidth);
+//   stroke(0, 0, 255, 10);
+//   fill(0, 255, 0, 10);
+//   square(200, 150, squareSize);
+// }
+
+// 8.2 Creating HTML elements with Javascript
+// function mousePressed() {
+//   //createP("My favorite color is red");
+//   createElement("p", "My name is Miguel");
+// }
+
+// 24/01/2023 8.3 Manipulating the DOM Elements with HTML and position
+// let canvas;
+// let h1;
+
+// function setup() {
+
+//   canvas = createCanvas(200, 200);
+//   canvas.position(400, 500);
+//   h1 = createElement("h1", "Waiting");
+//   //h1.position(400, 600);
+// }
+
+// function mousePressed(){
+//   createP("My favorite numbers below" + random(0,10));
+// }
+
+// function draw() {
+//   clear();
+//   background(255);
+//   fill(255, 0, 0);
+//   rect(100, 100, 50, 50);
+// }
+
+
+// 8.4 Handling DOM events with Callbacks
+let bgcolor;
+let button1;
+let button2;
+let slider;
+let nameInput;
+let nameP;
 
 function setup() {
-  createCanvas(500, 400);
-  //squareSize = random(50, 100);
-  lineWidth = random(4,28);
-  background(100);
+  canvas = createCanvas(200, 200);
+  bgcolor = color(200);
+  nameP = createP("Your name!");
+  button1 = createButton("go go go go");
+  button1.mousePressed(changeColor);
+  button2 = createButton("Empty field");
+  slider = createSlider(10, 100, 47);
+  nameInput = createInput("type your name");
+  nameP.mouseOver(overpara);
+  nameP.mouseOut(outpara);
+
+  nameInput.changed(updateText);
+  nameInput.input(updateText);
+  
+}
+
+function updateText() {
+  nameP.html(nameInput.value());
+}
+
+function overpara() {
+  nameP.html("Your mouse is over me!");
+}
+
+function outpara() {
+  nameP.html("Your mouse is out!");
+}
+
+function changeColor() {
+  bgcolor = color(random(255));
+}
+
+function mousePressed(){
+  //bgcolor = color(random(255));
+  changeColor();
 }
 
 function draw() {
-  //background(100);
-  squareSize = random(5, 250);
-
-  rectMode(CENTER);
-  strokeWeight(lineWidth);
-  stroke(0, 0, 255, 10);
-  fill(0, 255, 0, 10);
-  square(200, 150, squareSize);
-}
-
-// 8.2 Creating HTML elements with Javascript
-function mousePressed() {
-  //createP("My favorite color is red");
-  createElement("p", "My name is Miguel");
+  background(bgcolor);
+  fill(255, 0, 175);
+  ellipse(100, 100, slider.value(), slider.value());
+  //nameP.html(input.value());
+  //text(nameInput.value(), 10, 20);
 }
 
 
+// 8.5 Interacting with the DOM using sliders, buttons 
 
 
 
