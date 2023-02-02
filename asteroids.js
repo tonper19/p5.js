3529ad2aec88a383a1c65ff7e3dc052f6861e49a
Miguel Perez <miguel.p.donate@gmail.com>
2023-02-02 18:52:57 +0100

    progress Asteroids JS 02/02/2023

--- asteroids.js
+++ asteroids.js
@@ -1,4 +1,5 @@
 let ship;
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
     ship = new Ship();
@@ -7,22 +8,66 @@ function setup() {
 function draw() {
     background(0);
     ship.render();
+    ship.turn();
+    ship.update();
 }
 
+function keyReleased() {
+    ship.setRotation(0);
+    ship.boosting(false);
+}
+
+
 function keyPressed() {
-    if (keyCode == RIGHT_ARROW){
-        ship.turn(0.1);
-    } else if (keyCode == LEFT_ARROW){
-        ship.turn(-0.1);
+    if (keyCode === RIGHT_ARROW) {
+        ship.setRotation(0.1);
+    } else if (keyCode === LEFT_ARROW) {
+        ship.setRotation(-0.1);
+    } else if (keyCode === UP_ARROW) {
+        ship.boosting(true);
     }
 }
 
-function Ship() {
-    this.pos = createVector(width/2, height/2);
-    this.r = 20;
-    this.heading = PI / 2;
 
-    this.render = function() {
+class Ship {
+    constructor() {
+        this.pos = createVector(width / 2, height / 2);
+        this.r = 20;
+        this.heading = 0;
+        this.rotation = 0;
+        this.vel = createVector(1, 0);
+        this.isBoosting = false;
+
+        this.boosting = function(b){
+            this.isBoosting = b;
+        }
+    }
+
+    update() {
+        this.pos.add(this.vel);
+        if (this.isBoosting) {
+            this.boost();
+        }
+        if (this.pos.x > width) {
+            this.pos.x = 0;
+        } else if (this.pos.x < 0) {
+            this.pos.x = width;
+        }
+        if (this.pos.y > height) {
+            this.pos.y = 0;
+        } else if (this.pos.y < 0) {
+            this.pos.y = height;
+        }
+        this.vel.mult(0);
+    }
+
+    boost() {
+        let force = p5.Vector.fromAngle(this.heading);
+        force.mult(0.1);
+        this.vel.add(force);
+    }
+
+    render() {
         translate(this.pos.x, this.pos.y);
         rotate(this.heading);
         noFill();
@@ -30,10 +75,23 @@ function Ship() {
         triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
     }
 
-    this.turn = function(angle){
-        this.heading += angle;
+    setRotation(a) {
+        this.rotation = a;
+    }
+
+    turn() {
+        this.heading += this.rotation;
     }
 }
+
+
+
+// function setup() {
+//     createCanvas(windowWidth, windowHeight)
+// }
+
+// function draw
+
 
 
 
